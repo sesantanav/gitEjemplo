88e42cbe559f7626a14c7a2fd973d2380cbb1586
CastilloGitHub <gaspar.castillo@correoaiep.cl>
2022-05-05 21:23:00 -0400

    Getters and Setters astromeca

--- astromecanicos.dart
+++ astromecanicos.dart
@@ -3,4 +3,9 @@ import './droides.dart';
 class Astromecanicos extends Droides {
   String? serie;
   String? modelo;
+  
+  get getSerie => this.serie;
+  set setSerie( serie) => this.serie = serie;
+  get getModelo => this.modelo;
+  set setModelo( modelo) => this.modelo = modelo;
 }
